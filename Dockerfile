67d866c82bb22996036b5483dd3ce4845760e241
Henrik Lynbech <henrik@emento.dk>
2026-04-24 15:58:56 +0200

    enabling apm

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ ENV DD_ENV="staging"
 ENV DD_LOGS_INJECTION=true
 ENV DD_GIT_COMMIT_SHA=${GIT_COMMIT}
 ENV DD_GIT_REPOSITORY_URL="github.com/hlynbech2/henrik-test-datadog-repo-integration2"
+ENV DD_SERVICE="henriktestserver"
 
 WORKDIR /app
 
--- server.js
+++ server.js
@@ -1,44 +1,44 @@
-const tracer = require('dd-trace').init()
+const tracer = require("dd-trace").init({
+  hostname: "dd-agent",
+});
 
+async function run() {
+  function myLogger(message) {
+    // set SERVER_VERSION to the current git commit hash
+    console.log(
+      `${new Date().toISOString()} - henriktestserver version ${process.env.GIT_COMMIT} - ${message}`,
+    );
+  }
 
-async function run () {
-    
-    function myLogger(message) {
-        // set SERVER_VERSION to the current git commit hash
-        console.log(`${new Date().toISOString()} - henriktestserver version ${process.env.GIT_COMMIT} - ${message}`);
+  function f1() {
+    myLogger("f1 called");
+  }
+
+  function f2() {
+    myLogger("f2 called");
+  }
+
+  function f3() {
+    x = y; // this will throw an error since y is not defined, to test error logging in datadog
+    myLogger("f3 called");
+  }
+
+  const myFunctions = [f1, f2, f3];
+
+  while (true) {
+    // Pick a random function from the array and call it
+    const randomIndex = Math.floor(Math.random() * myFunctions.length);
+
+    try {
+      myFunctions[randomIndex]();
+    } catch (error) {
+      myLogger(`Error occurred: ${error.stack}`);
     }
 
-    function f1(){
-        myLogger('f1 called');
-    };
-
-    function f2(){
-        myLogger('f2 called');
-    }
-
-    function f3(){
-        x=y; // this will throw an error since y is not defined, to test error logging in datadog
-        myLogger('f3 called');
-    }
-
-    const myFunctions = [f1, f2, f3];
-
-    while (true) {
-        // Pick a random function from the array and call it
-        const randomIndex = Math.floor(Math.random() * myFunctions.length);
-
-        try {
-            myFunctions[randomIndex]();
-        } catch (error) {
-            myLogger(`Error occurred: ${error.stack}`);
-        }
-        
-        
-        //wait a random amount of time of a few seconds before calling the next function
-        const randomDelay = Math.floor(Math.random() * 3000) + 100;
-        await new Promise(resolve => setTimeout(resolve, randomDelay));
-    }
+    //wait a random amount of time of a few seconds before calling the next function
+    const randomDelay = Math.floor(Math.random() * 3000) + 100;
+    await new Promise((resolve) => setTimeout(resolve, randomDelay));
+  }
 }
 
 run();
-
